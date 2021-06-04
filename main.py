88483862d5c94c113d5258c1d2629b5b75443184
williamjunqueira42 <williamnutella42@gmail.com>
2021-06-04 18:00:53 -0300

    Adicionando codigo principal no GitHub

---
+++ main.py
@@ -0,0 +1,24 @@
+# Teste de deselvonvimento de sistemas
+# desenvolvindo por William, ...
+
+
+
+from tkinter import *
+from pygame import *  #  tem que instalar a blibioteca no seu pc
+
+# Obs vamos usar todas as funçoes com nome em ingles 
+
+class mp3():
+    def __init__(self, master):
+        self.master = master
+
+# Codigo Principal
+
+root =Tk()
+mp3(root)
+
+root.iconbitmap('favicon.ico')
+root.title('Mp3')
+
+root.mainloop()
+
