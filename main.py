37726b60e7245ac5e86737712b45d65abce15795
williamjunqueira42 <williamnutella42@gmail.com>
2021-06-07 17:34:27 -0300

    update main.py

--- main.py
+++ main.py
@@ -2,77 +2,78 @@
 # desenvolvindo por William, Lucas Leal, Pedro Damazio ...
 
 
-
 from tkinter import * 
 from pygame import *  #  tem que instalar a blibioteca no seu pc
-from eyed3 import *  # para pegar os metadados das musicas, tabem tem que installar
-import tkinter.ttk as ttk
+from eyed3 import *  # para pegar os metadados das musicas, tabem tem que installar 
 from time import *
 from mutagen.mp3 import MP3
 from tkinter import filedialog
  
 
-
 class mp3():
     def __init__(self, master): 
         self.master = master
-        
+        #  criando os frames
         self.frameLabels = Frame(self.master, bg='black')  #  frame dos labels 
         self.frameButtons = Frame(self.master, bg='black') #  frames de botões
         self.frameSlide = Frame(self.master, bg='black')
         self.framelistBox = Frame(self.master, bg='white')
         
+        # posicionando os frames
         self.frameButtons.pack(side=BOTTOM, pady=30)
         self.frameSlide.pack(side=BOTTOM, anchor=S)
         self.frameLabels.pack(side=BOTTOM, pady=30)
         self.framelistBox.pack(side = BOTTOM)
-        
 
-        # Menu 
+#--------------------------------------------------------------------------------
+# ----------------------------------MENU------------------------------------------
+#---------------------------------------------------------------------------------
         
-        self.menu = Menu(self.frameLabels,)
+        self.menu = Menu(self.frameLabels,) #  Criação do menu
         self.master.config(menu=self.menu)
+
         self.song_menu = Menu(self.menu)
         self.menu.add_cascade(label="Add songs", menu=self.song_menu)
         self.song_menu.add_command(label="Add one song", command=self.addsong)
 
-        self.song = ('musicas/⠀.mp3')
-        
-        # -----Labels---------
-
-        self.musicanome = load(self.song).tag.title
-        self.artistanome = load(self.song).tag.album_artist
-
-
-        #adicionado labels
+#--------------------------------------------------------------------------------
 
         self.lista = Listbox(self.framelistBox, bg='white', fg='black', width=60, height=20, selectbackground='black')
-        
-
-
-   
-        self.lbnomemusica = Label(self.frameLabels, text=f'{self.musicanome}', fg='white', bg='black', font='Coolvetica 20 bold')
-
-        self.lbnomeartista = Label(self.frameLabels, text=f'{self.artistanome}', fg='white', bg='black', font='Coolvetica 12 bold')
-
-        # adicionado labels na janela
-
-
         self.lista.pack()
+
+
+#---------------------------------------------------------------------------------
+#----------------------------------LABELS-----------------------------------------#---------------------------------------------------------------------------------
+        
+         #  variaveis referentes ao nome da musica e nome do cantor
+        self.musicanome = StringVar() 
+        self.artistanome = StringVar()
+        self.musicanome.set('')
+        self.artistanome.set('')
+
+     
+        #adicionado os labels
+
+        self.lbnomemusica = Label(self.frameLabels, textvariable=self.musicanome, fg='white', bg='black', font='Coolvetica 20 bold')
+
+        self.lbnomeartista = Label(self.frameLabels, textvariable=self.artistanome, fg='white', bg='black', font='Coolvetica 12 bold')
+
+        # posicionando os labels
+        
         self.lbnomemusica.pack(anchor=W)
         self.lbnomeartista.pack()
         
-
+#----------------------------------------------------------------------------
 
         self.status_bar = Label(self.frameSlide, text='00:00:00', anchor=E, bg='black', fg='white')
         self.status_bar.pack(fill=X, padx=20)
 
 
 
+#---------------------------------------------------------------------------------
+#----------------------------------BUTTONS----------------------------------------#---------------------------------------------------------------------------------
 
-        # -----Buttons--------
-
-        # imagens dos botões
+        #Imagens dos botões
         
         self.img_btstart = PhotoImage(file='imagens/btstart.png')
         self.img_btpause = PhotoImage(file='imagens/btpause.png')
@@ -82,6 +83,7 @@ class mp3():
 
 
         # adicionado os botões
+
         self.btstart = Button(self.frameButtons, image=self.img_btstart, width=50, height=50, bg='black', activebackground='black', relief='flat', command=self.playPausesong)
 
         self.btstop = Button(self.frameButtons, image=self.img_btstop, width=20, height=30, bg='black', activebackground='black', relief='flat', command=self.stopsong)
@@ -97,27 +99,31 @@ class mp3():
         self.btavanced.imagem = self.img_btavanced
 
 
-        # adicionado os botões no frameButtons
+        # posicionando os botões
     
         self.btreturn.pack(side = LEFT, padx=8)
         self.btstart.pack(side = LEFT, padx=8)
         self.btstop.pack(side = LEFT, padx=8) 
         self.btavanced.pack(side = LEFT, padx=8)
-    
+#---------------------------------------------------------------------------------
         self.v = 1
         self.mixer = mixer
         
     def playPausesong(self):  #  metodo para tocar a musica
+
+
+        self.mixer.init()  #  iniciando mixer pygame
         
-        self.indice = self.lista.curselection()
-        self.texto = self.lista.get(self.indice)
+        self.musicanome.set(load(self.song).tag.title)
+        self.artistanome.set(load(self.song).tag.album_artist)
+
+        
         if self.v == 1:
 
             #  mudando o botão play para o botão pause
             self.img_btstart = PhotoImage(file='imagens/btpause.png')
             self.btstart['image'] = self.img_btpause
-
-            self.mixer.init()
+       
             self.mixer.music.load(f'{self.song}')
             self.mixer.music.play()
             self.v = 2
@@ -130,8 +136,11 @@ class mp3():
             self.btstart['image'] = self.img_btpause
             self.mixer.music.unpause()
             self.v = 2
+        
         self.playTime()
 
+
+        
 
     def stopsong(self):  #  metodo para parar de tocar a musica
     
@@ -164,6 +173,7 @@ class mp3():
         self.song = filedialog.askopenfilename(initialdir='musicas/', title='escolha algum som', )
 
         self.lista.insert(END, load(self.song).tag.title)
+        self.stopsong()
        
         
 
